4e9d2855a46ec2e6cdc71936f6357688cabd5830
Richard Shade <rshade@flexera.com>
2021-04-29 11:32:53 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+test_deps = [
+    'coverage',
+    'pytest',
+    'pylint',
+    'flake8'
+]
+
+extras = {
+    'test': test_deps,
+}
+
+setup(
+    # Other metadata...
+    tests_require=test_deps,
+    extras_require=extras,
+)
