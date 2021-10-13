f0012a54201393b11e1c8ee918c1c883756a7b7b
Richard Shade <rshade@flexera.com>
2021-10-13 10:32:21 -0500

    create_bill_connect

--- cco/cbi/create_bill_connect.py
+++ cco/cbi/create_bill_connect.py
@@ -1,36 +1,8 @@
-'''
-Bill Connect Tool
----------------------------
-Creates a bill connect and returns the response
-Usage: python create_bill_connect.py <refresh_token> <shard> <org_id> <cbi_integration_id> <cbi_bill_identifier> <cbi_name> <cbi_params> true
-
-Parameters:
-<refresh token>:        obtained from Cloud Management:
-                        - go to Settings, then API Credentials in the Account Settings;
-                        - enable the token and pass its value to this script.
-<shard>:                3 or 4 for cm auth, F1 for FlexeraOne
-<org_id>:               the relevant organization id, e.g. "12345"
-<cbi_integration_id>:   the integration id (linked to that integration mentioned just above), e.g. "cbi-oi-optima";
-                        - Possible integration ids: “cbi-oi-optima” (Optima CSV default format),
-                        - but also "cbi-oi-oracle", “cbi-oi-alibaba” and “cbi-oi-azure-china”
-                        - (talk to your Flexera account manager for those last three);
-<cbi_bill_identifier>:  a bill identifier of your choice, alphanumeric (or - or _)
-                        - sequence uniquely identifying this bill connect of this integration type for your organization, e.g. "test-1";
-<cbi_name>:             a name/description, a human-readable string to give more information, e.g. "My test Optima CSV integration, number One";
-<cbi_params>:           a parameter object in json to override the default integration settings. Pass an empty one if you are not passing any such parameters,
-                        e.g. '{}'.
-<tls_verification>:     true/false
-'''
-import os
 import json
 import logging
 import requests
 import sys
-import time
-import urllib3
-from urllib3.exceptions import InsecureRequestWarning
 import click
-import pprint
 
 # Tweak the destination (e.g. sys.stdout instead) and level (e.g. logging.DEBUG instead) to taste!
 logging.basicConfig(format='%(levelname)s:%(asctime)s:%(message)s', stream=sys.stderr, level=logging.INFO)
@@ -61,7 +33,7 @@ def create_bill_connect(refresh_token, host, org_id, id, name, integration_id, o
     auth_headers = {"Api-Version": "1.0", "Authorization": "Bearer " + access_token}
     kwargs = {"headers": auth_headers, "allow_redirects": False}
 
-    bill_connect_url = "https://onboarding.rightscale.com/api/onboarding/orgs/{}/bill_connects/cbi".format(org_id)
+    bill_connect_url = "https://api.optima.flexeraeng.com/api/onboarding/orgs/{}/bill_connects/cbi".format(org_id)
     cbi_params = {}
     if optima_display_name:
         cbi_params["optima_display_name"] = optima_display_name
