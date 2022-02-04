9bf108c273dcb85c22e78f69259826c13c7c89ee
Richard Shade <rshade@flexera.com>
2022-02-04 15:47:18 -0600

    fixing lint

--- cli-example.py
+++ cli-example.py
@@ -1,17 +1,19 @@
-#cli.py
+# cli.py
 import click
-from .bar import make_bar
+# from .bar import make_bar
 
 @click.group()
 def foo():
     pass
 
-make_bar(foo)
+# make_bar(foo)
+
+
 foo()
 
 
-#bar.py
-from .baz import make_baz
+# bar.py
+# from .baz import make_baz
 
 def make_bar(foo):
     @foo.group()
@@ -20,7 +22,7 @@ def make_bar(foo):
 
     make_baz(bar)
 
-#baz.py
+# baz.py
 
 def make_baz(bar):
     @bar.command()
